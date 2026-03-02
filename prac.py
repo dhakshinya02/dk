ebbe3cb369d282407f7f8926ced7076466409628
dhakshinya02 <dhakshinya.mail02@gmail.com>
2026-03-02 11:41:13 +0530

    Add files via upload
    a file is uploadd

--- prac.py
+++ prac.py
@@ -1,1 +1,13 @@
-
+def clist():
+    l,l1,l2=[],[],[]
+    for i in range(1,10):
+        l.append(i)
+    for i in range(10,1,-2):
+        l1.append(len(l1))
+    for i in range(len(l1)):
+        l2.append(l1[i]+l[i])
+    l2.append(len(l)-len(l1))
+    print(l)
+    print(l1)
+    print(l2)
+clist()
